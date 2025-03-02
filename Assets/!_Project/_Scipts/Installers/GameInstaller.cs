03dc56fb182a2e96af6b628ae4413200ed6270fa
Andriy <andriydulienko@gmail.com>
2025-03-02 19:58:34 +0200

    TMP

--- Assets/!_Project/_Scipts/Installers/GameInstaller.cs
+++ Assets/!_Project/_Scipts/Installers/GameInstaller.cs
@@ -3,10 +3,14 @@ using Zenject;
 
 namespace Game
 {
+    /// <summary>
+    /// Binds dependencies in the game state
+    /// </summary>
     public class GameInstaller : MonoInstaller
     {
         public override void InstallBindings()
         {
+            Container.Bind<GameManager>().FromComponentInHierarchy().AsSingle();
         }
     }
 }
