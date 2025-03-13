8cf218f7be5822e03e373eceb4b2e3c49410f027
Andriy <andriydulienko@gmail.com>
2025-03-13 12:18:02 +0200

    Derive SampleSO from ScriptableObject

--- Assets/!_Project/_Scipts/Data/ScriptableObjects/SampleSO.cs
+++ Assets/!_Project/_Scipts/Data/ScriptableObjects/SampleSO.cs
@@ -3,7 +3,7 @@
 namespace Game
 {
     [CreateAssetMenu(fileName = "SampleSO", menuName = "Game/SampleSO", order = 0)]
-    public class SampleSO : MonoBehaviour
+    public class SampleSO : ScriptableObject
     {
 
     }
